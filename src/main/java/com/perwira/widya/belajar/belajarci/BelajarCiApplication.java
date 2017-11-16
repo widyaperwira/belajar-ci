37f925c1a9f1f62a593caef71215cf115e120b49
widyaperwira <widyaperwira@gmail.com>
2017-11-16 17:55:02 +0700

    template project

---
+++ src/main/java/com/perwira/widya/belajar/belajarci/BelajarCiApplication.java
@@ -0,0 +1,12 @@
+package com.perwira.widya.belajar.belajarci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarCiApplication.class, args);
+	}
+}
